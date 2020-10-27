8c588a7b442c3b2fe953f3088930a807dc10548a
Bartlomiej Korytkowski <korytkowskibartlomiej@gmail.com>
2020-10-27 12:35:04 +0100

    Updates

---
+++ precache-manifest.6589903f10c147606156eebd85abaf78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67beb593fc6a136d00844f9fe1ee1e1",
+    "url": "/CurrentExchangeReact/index.html"
+  },
+  {
+    "revision": "f829ef1bac8c630bdbfa",
+    "url": "/CurrentExchangeReact/static/css/main.5c74af25.chunk.css"
+  },
+  {
+    "revision": "72e58375e9de9502ab30",
+    "url": "/CurrentExchangeReact/static/js/2.7a6553bd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CurrentExchangeReact/static/js/2.7a6553bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f829ef1bac8c630bdbfa",
+    "url": "/CurrentExchangeReact/static/js/main.5583147f.chunk.js"
+  },
+  {
+    "revision": "aa9e7204f8075a4dd359",
+    "url": "/CurrentExchangeReact/static/js/runtime-main.d1e6feab.js"
+  }
+]);
