941cfbb0f3729706f52a1178e137e284f58b3dce
Bartlomiej Korytkowski <korytkowskibartlomiej@gmail.com>
2020-10-28 00:29:05 +0100

    add SWITCH

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App  exact path="/" />
   </React.StrictMode>,
   document.getElementById('root')
 );
