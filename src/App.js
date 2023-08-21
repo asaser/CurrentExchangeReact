9dbf3d1b5b87626a8fcef3d08d10215e049b3287
bartlomiej <bartlomiej@euroloop.tech>
2023-08-21 16:59:57 +0200

    create new api connection

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  // Link,
-  Route
-} from 'react-router-dom';
+import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navigation from './components/Navigations/Navigation';
+// import Navigation from "./components/Navigations/Navigation";
 
-import Calculator from '../src/pages/Calculators/Calculator';
-import './App.css';
-
-
-
+import Calculator from "../src/pages/Calculators/Calculator";
+import "./App.css";
 function App() {
-  // JSX
   return (
     <div className="App">
-
-      <Router>
-          <Navigation></Navigation>
-          <Switch>
-            <Route>
-            <Calculator></Calculator>
-            </Route>
-            {/* <Route path="/flags">
-            <Flags></Flags>
-            </Route> */}
-          </Switch>
-        </Router>
-
+      <Calculator />
     </div>
   );
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Navigation></Navigation>
+//         <Switch>
+//           <Route>
+//             <Calculator></Calculator>
+//           </Route>
+//         </Switch>
+//       </Router>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Navigations/Navigation.js
+++ src/components/Navigations/Navigation.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import './Navigation.css';
-
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import "./Navigation.css";
+import logoNavBar from "../../images/logoNavbar.png";
 
 function Navigation() {
+  const navbarText = "Current Exchange Value";
 
-    const navbarText = "Current Exchange Value";
-
-    return (
-        <Navbar className="mainNav" fixed="top" bg="transparent" variant="transparent">
-            <Navbar.Brand>
-                <img
-                    src="./logoNavbar.png"
-                    alt=""
-                    width="30px"
-                    height="30px"
-                    className="d-inline-block align-top mainLogoNav"
-                />
-             <b>{navbarText}</b>
-            </Navbar.Brand>
-        </Navbar>
-    );
+  return (
+    <Navbar
+      className="mainNav"
+      fixed="top"
+      bg="transparent"
+      variant="transparent"
+    >
+      <Navbar.Brand>
+        <img
+          src={logoNavBar}
+          alt=""
+          width="30px"
+          height="30px"
+          className="d-inline-block align-top mainLogoNav"
+        />
+        <b>{navbarText}</b>
+      </Navbar.Brand>
+    </Navbar>
+  );
 }
 
 export default Navigation;
